b45f70d547d0cecf70a0c237326ca806b5d4bd46
HAO <147186777+KARENLHAO@users.noreply.github.com>
2026-02-04 06:19:47 +0000

    support >=3.8

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-rm -rf tmp report.html result.csv
 
-python -m peptide_cutter --fasta /workspaces/peptide_cutter/G3P_HUMAM_P04406.fasta --enzymes all --out report.html
+
+python -m peptide_cutter --fasta G3P_HUMAM_P04406.fasta --enzymes all --out report.html
